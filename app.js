2973a64a4395195db260e721139d39921ac8ea14
Daniel Cortez <daniel@jyde.io>
2021-12-01 17:51:54 +0100

    Improved control flow

--- app.js
+++ app.js
@@ -21,67 +21,83 @@ import {
 
     while (true) {
       console.log(`Attempt at: ${new Date().toISOString()}`);
+
       // Get available listings from WG-Gesucht based on filters.
       const crawledListings = await crawlListings(process.env.FILTER_URL);
+
       // Write available listings to the DB if they are not already there.
-      crawledListings.forEach(async (listing) => {
+      await crawledListings.forEach(async (listing) => {
         const listingInDb = await findListingById(db, listing.id);
         if (!listingInDb) {
           await writeListing(db, listing);
         }
       });
+
       // Check DB for any listings we haven't written to yet.
       const listingsNotMessaged = await getListingsNotMessaged(db);
-      if (listingsNotMessaged.length > 0) {
-        // Create request headers with login cookie.
-        const loggedInHeaders = {
-          "Content-Type": "application/json",
-          "User-Agent":
-            "Mozilla/5.0 (Linux; Android 10; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.62 Mobile Safari/537.36",
-          Cookie: await getLoginCookie(
-            process.env.WG_USER,
-            process.env.WG_PASSWORD
-          ),
-          Origin: "https://www.wg-gesucht.de",
-          Host: "www.wg-gesucht.de",
-        };
-        // Get up-to-date message templates from WG-Gesucht profile.
-        const messageTemplates = await getMessageTemplates(loggedInHeaders);
-        // Construct a message payload and send it for each listing not messaged in DB.
-        await listingsNotMessaged.forEach(async (listing) => {
-          // Get necessary data to send a message from the listing URL.
-          let csrfToken, userId;
-          try {
-            ({ csrfToken, userId } = await getMessageData(
-              listing.longId,
-              loggedInHeaders
-            ));
-          } catch (e) {
-            console.log(`getMessageData failed with message ${e.message}`);
-            await removeListingById(db, listing.id);
-            return; // Skip the rest of this sequence when necessary data is not retrieved from URL.
-          }
-          // Choose a message to send based on interpreted language of the listing description.
-          const message =
-            listing.lang === "eng"
-              ? messageTemplates.eng
-              : messageTemplates.ger;
-          // Send the message to the user who owns the listing.
-          const messageSent = await sendMessage(
-            listing.id,
-            csrfToken,
-            userId,
-            message,
-            loggedInHeaders
-          );
-          // On success, mark the listing as messaged in the DB.
-          if (messageSent) {
-            await updateListingById(db, listing.id, { sent: 1 });
-          }
-        });
+
+      // If no new listings were crawled, sleep and skip messaging.
+      if (listingsNotMessaged.length === 0) {
+        console.log(
+          `No new listings found. Sleeping for ${process.env.WAIT_SECONDS} seconds.`
+        );
+        await sleep(parseInt(process.env.WAIT_SECONDS) * 1000);
+        continue;
       }
+
+      // If new listings were crawled, send a message request to the listing owner.
+      // Create request headers with login cookie.
+      const loggedInHeaders = {
+        "Content-Type": "application/json",
+        "User-Agent":
+          "Mozilla/5.0 (Linux; Android 10; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.62 Mobile Safari/537.36",
+        Cookie: await getLoginCookie(
+          process.env.WG_USER,
+          process.env.WG_PASSWORD
+        ),
+        Origin: "https://www.wg-gesucht.de",
+        Host: "www.wg-gesucht.de",
+      };
+
+      // Get up-to-date message templates from WG-Gesucht profile.
+      const messageTemplates = await getMessageTemplates(loggedInHeaders);
+
+      // Construct a message payload and send it for each listing not messaged in DB.
+      await listingsNotMessaged.forEach(async (listing) => {
+        // Get necessary data to send a message from the listing URL.
+        const { csrfToken, userId } = await getMessageData(
+          listing.longId,
+          loggedInHeaders
+        ).catch((e) => {
+          console.log(`getMessageData failed with error message: ${e.message}`);
+        });
+
+        // If necessary data not found, remove listing from DB and skip sending message.
+        if (!csrfToken || !userId) {
+          await removeListingById(db, listing.id);
+          return;
+        }
+
+        // Choose a message to send based on interpreted language of the listing description.
+        const message =
+          listing.lang === "eng" ? messageTemplates.eng : messageTemplates.ger;
+
+        // Send the message to the user who owns the listing.
+        const messageSent = await sendMessage(
+          listing.id,
+          csrfToken,
+          userId,
+          message,
+          loggedInHeaders
+        );
+
+        // On success, mark the listing as messaged in the DB.
+        if (messageSent) {
+          await updateListingById(db, listing.id, { sent: 1 });
+        }
+      });
       console.log(
-        `Attempt finished. Sleeping for ${process.env.WAIT_SECONDS} seconds.`
+        `Messaged the owners of new listings. Sleeping for ${process.env.WAIT_SECONDS} seconds.`
       );
       await sleep(parseInt(process.env.WAIT_SECONDS) * 1000);
     }
@@ -105,6 +121,7 @@ async function findListingById(db, listingId) {
 }
 
 async function writeListing(db, listing) {
+  await db.read();
   db.data.listings.push(listing);
   await db.write();
   console.log("Added listing to DB:", JSON.stringify(listing, null, 2));
